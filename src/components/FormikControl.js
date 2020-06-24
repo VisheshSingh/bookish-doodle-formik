f68402e78770acf35616bb411253690a37cb2122
Vishesh Singh <VisheshSingh16@gmail.com>
2020-06-23 20:25:53 -0500

    Formik control comp 🤹‍♀️

---
+++ src/components/FormikControl.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FormikControl = (props) => {
+  const { control } = props;
+
+  switch (control) {
+    case 'input':
+    case 'textarea':
+    case 'select':
+    case 'radio':
+    case 'checkbox':
+    case 'date':
+    default:
+      return null;
+  }
+  return <div></div>;
+};
+
+export default FormikControl;
